632bff44918fdf1c4deb68f69b755fcd40174609
m-nikhil <m_nikhil@outlook.com>
2022-05-28 00:20:17 -0700

    update host

--- app.py
+++ app.py
@@ -265,4 +265,4 @@ def bookingConfirm(date, attraction_id):
     return render_template('ConfirmBooking.html', message="Booking Confirmed")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
